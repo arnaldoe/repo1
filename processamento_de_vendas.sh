3c75caab72e4d5ef03e9fff96f9971463cc99e39
Naldo Espanhol <900arnaldo@gmail.com>
2024-04-05 15:47:56 -0300

    novo sh

--- processamento_de_vendas.sh
+++ processamento_de_vendas.sh
@@ -146,8 +146,13 @@ mv vendas/backup/dados-$data.csv vendas/backup/backup-dados-$data.csv
 sed -i '57s/\,/./4' dados_de_vendas.csv
 sed -i '68s/31\/01\/2023/31\/03\/2023/' dados_de_vendas.csv
 
-first_sale_date=$(grep '^[0-9]' dados_de_vendas.csv | awk -F ',' 'BEGIN {OFS=","} {if (NF == 5 && $NF ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {split($NF, d, "/"); print d[3] d[2] d[1],$0} else if (NF == 6 && $(NF-1) ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {split($(NF-1), d, "/"); print d[3] d[2] d[1],$0}}' | head -n 1 | cut -d',' -f6)
-last_sale_date=$(grep '^[0-9]' dados_de_vendas.csv | awk -F ',' 'BEGIN {OFS=","} {if (NF == 5 && $NF ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {split($NF, d, "/"); print d[3] d[2] d[1],$0} else if (NF == 6 && $(NF-1) ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {split($(NF-1), d, "/"); print d[3] d[2] d[1],$0}}' | tail -n 1 | cut -d',' -f6)
+#first_sale_date=$(grep '^[0-9]' dados_de_vendas.csv | awk -F ',' 'BEGIN {OFS=","} {if (NF == 5 && $NF ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {split($NF, d, "/"); print d[3] d[2] d[1],$0} else if (NF == 6 && $(NF-1) ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {split($(NF-1), d, "/"); print d[3] d[2] d[1],$0}}' | head -n 1 | cut -d',' -f6)
+#last_sale_date=$(grep '^[0-9]' dados_de_vendas.csv | awk -F ',' 'BEGIN {OFS=","} {if (NF == 5 && $NF ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {split($NF, d, "/"); print d[3] d[2] d[1],$0} else if (NF == 6 && $(NF-1) ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) {split($(NF-1), d, "/"); print d[3] d[2] d[1],$0}}' | tail -n 1 | cut -d',' -f6)
+
+first_sale_date=$(awk -F ',' '$1 ~ /^[0-9]+$/ && NF == 5 && $NF ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/ {print $NF}' dados_de_vendas.csv | sort | head -n 1)
+last_sale_date=$(awk -F ',' '$1 ~ /^[0-9]+$/ && NF == 5 && $NF ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/ {print $NF}' dados_de_vendas.csv | sort | tail -n 1)
+
+
 total_unique_items=$(grep '^[0-9]' vendas/dados_de_vendas.csv | cut -d',' -f2 | sort | uniq | wc -l)
 
 # Cria o arquivo "relatorio.txt" com as informações solicitadas
